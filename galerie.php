00814f43617e9df4199f53a75a1f44ab14cc59af
JeremieABJT <jeremy.aubrejat@gmail.com>
2024-02-06 15:39:07 +0100

    06.02.24

--- galerie.php
+++ galerie.php
@@ -10,87 +10,88 @@
             <div class="carousel-inner flex">
                 <!-- Première diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la première diapositive -->
+                   
                     <img src="img/1(3).jpg"class="w-1/2 max-w-[50%] md:w-auto h-auto"
-                        alt="Image 1">
+                        alt="represente une serviette">
                 </div>
                 <!-- Deuxième diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la deuxième diapositive -->
+                    
                     <img src="img/1(4).jpg"class="w-1/2 md:w-auto h-auto"
-                        alt="Image 2">
+                        alt="represente un t-shirt">
                 </div>
                 <!-- Troisième diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la troisième diapositive -->
+                 
                     <img src="img/1(2).jpg"class="w-1/2 md:w-auto h-auto"
-                        alt="Image 3">
+                        alt="represente une horloge">
                 </div>
                 <!-- Quatrième diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la quatrième diapositive -->
+                  
                     <img src="img/1(5).jpg"
-                        alt="Image 4">
+                        alt="poignée de baby-foot">
                 </div>
                 <!-- Cinquième diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la cinquième diapositive -->
+                    
                     <img src="img/1(6).jpg"
-                        alt="Image 5">
+                        alt="represente une plaque route 66">
                 </div>
-                <!-- Cinquième diapositive -->
+                <!-- Sixième diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la cinquième diapositive -->
+                 
                     <img src="img/1(7).jpg"
-                        alt="Image 6">
+                        alt="reprensente un lavabo en bois">
                 </div>
-                <!-- Cinquième diapositive -->
+                <!-- 7eme diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la cinquième diapositive -->
+                  
                     <img src="img/1(8).jpg"
-                        alt="Image 7">
+                        alt="represente une plaque avec un dragon">
                 </div>
-                <!-- Cinquième diapositive -->
+                <!-- 8eme diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la cinquième diapositive -->
+                  
                     <img src="img/1(10).jpg"
-                        alt="Image 8">
+                        alt="represente une horloge">
                 </div>
-                <!-- Cinquième diapositive -->
+                <!-- 9eme diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la cinquième diapositive -->
+                    
                     <img src="img/1(13).jpg"
-                        alt="Image 9">
+                        alt="represente des objets imprimés en 3d">
                 </div>
-                <!-- Cinquième diapositive -->
+                <!-- 10eme diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la cinquième diapositive -->
+                   
                     <img src="img/1(14).jpg"
-                        alt="Image 10">
+                        alt="represente des objets imprimés en 3d">
                 </div>
-                <!-- Cinquième diapositive -->
+                <!-- 11eme diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
                     <!-- Image de la cinquième diapositive -->
                     <img src="img/1(15).jpg"
-                        alt="Image 11">
+                        alt="represente un capteur de distance">
                 </div>
-                <!-- Cinquième diapositive -->
+                <!-- 12eme diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la cinquième diapositive -->
+                    
                     <img src="img/1(16).jpg"
-                        alt="Image 12">
+                        alt="écusson brodé avec une voiture">
                 </div>
-                <!-- Cinquième diapositive -->
+                <!-- 13eme diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la cinquième diapositive -->
+                   
                     <img src="img/1(17).jpg"
-                        alt="Image 13">
-                        <!-- Cinquième diapositive -->
+                        alt="écusson brodé avec une voiture">
+                       
                 </div>
+                <!-- 14eme diapositive -->
                 <div class="w-full md:w-auto h-auto slide">
-                    <!-- Image de la cinquième diapositive -->
+                  
                     <img src="img/1(18).jpg"
-                        alt="Image 14">
+                        alt="écusson brodé avec une voiture">
                 </div>
             </div>
             <!-- Conteneur pour les boutons de navigation -->
@@ -106,4 +107,5 @@
     </div>
 
 <?php include ('src/inc/footer.php');?>
+<script type="text/javascript" src="testCarousel.js" async></script>
 </body>
--- src/inc/header.php
+++ src/inc/header.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="src/output.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="src/output.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap">
     <link rel="stylesheet" href="src/css/testCarousel.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <!-- Lien vers le fichier JavaScript pour les fonctionnalités interactives -->
-    <script src="fabLab.js" async></script>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script type="text/javascript" src="testCarousel.js" async></script>
+    
+    <!--<script src="https://cdn.tailwindcss.com"></script>-->
     <title>Fablab18</title>
 </head>
 
@@ -69,5 +70,5 @@
 
 
 <!--fin du header-->
-</html>
 
+</html>
