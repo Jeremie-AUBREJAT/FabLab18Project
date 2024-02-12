ff62d45cd3f42216782a2af75bd66fada8e930fd
JeremieABJT <jeremy.aubrejat@gmail.com>
2024-02-12 14:10:53 +0100

    12.02/14:10

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -4,22 +4,23 @@
 <footer class="bg-gray-300 text-white py-4" style="background-image: url('img/testx2.png'); background-size: cover;" loading="lazy">
     <div class="container mx-auto flex flex-wrap justify-between items-center">
         <div class="flex items-center">
-        <a href="index.php">
-           <img src="img/1.png" alt="Logo" class="w-32 h-32 mx-10" loading="lazy">
-           <a href="index.php">
-           <div class="text-black font-permanent-marker w-8 h-8 text-2xl font-bold mr-10">BerryLab18           
-            </div>
+            <a href="index.php">
+                <img src="img/1.png" alt="Logo" class="w-32 h-32 mx-10" loading="lazy">
+            </a>
+            <a href="index.php">
+                <div class="text-black font-permanent-marker w-8 h-8 text-2xl font-bold mr-10">BerryLab18           
+                </div>
+            </a>
         </div>
   
         <div class="invisible lg:visible lg:mr-24">
             <div class="text-center justify-center mb-6">          
-                <a href="mentions-legales.html" class="border-solid border-2 border-gray-600 text-black text-xl font-semibold bg-gray-100 hover:text-white hover:bg-gray-600 transition-all duration-300 px-4 py-2 rounded-2xl shadow-xl transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">Mentions légales</a>
-
-            </div>
-  
+                <a href="mentions-legales.html" class="border-solid border-2 border-gray-600 text-black text-xl font-semibold bg-gray-100 hover:text-white hover:bg-gray-600 transition-all duration-300 px-4 py-2 rounded-2xl shadow-xl transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">Mentions légales
+                </a>
+            </div> 
             <div class="text-center justify-center"> 
-                <a href="contact.html" class="border-solid border-2 border-gray-600 text-black text-xl font-semibold bg-gray-100 hover:text-white hover:bg-gray-600 transition-all duration-300 px-4 py-2 rounded-2xl shadow-xl transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">Contact</a>
-
+                <a href="contact.html" class="border-solid border-2 border-gray-600 text-black text-xl font-semibold bg-gray-100 hover:text-white hover:bg-gray-600 transition-all duration-300 px-4 py-2 rounded-2xl shadow-xl transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none">Contact                  
+                </a>
             </div>
         </div>
   
@@ -28,13 +29,11 @@
             <a href="https://www.facebook.com/?locale=fr_FR" class="text-xl text-black hover:text-gray-400">
                 <img src="img/pngegg(9).png" alt="facebook" class="w-8 h-8" loading="lazy">
                 <i class="fab fa-facebook"></i>
-            </a>
-            
+            </a>            
             <a href="https://twitter.com/?lang=fr" class="text-xl text-black hover:text-gray-400">
                 <img src="img/twitter-x-logo-42554.png" alt="X" class="w-8 h-8" loading="lazy">
                 <i class="fab fa-twitter"></i>
-            </a>
-           
+            </a>           
             <a href="https://www.instagram.com/" class="text-xl text-black hover:text-gray-400">
                 <img src="img/1(7).png" alt="instagram" class="w-8 h-8" loading="lazy">
                 <i class="fab fa-instagram"></i>
@@ -42,9 +41,9 @@
         </div>
     </div>
     <div class="fixed bottom-0 right-0 flex justify-center items-center">
-    <button class="scroll-to-top-button">
-        <img src="img/fleche.png" alt="bouton-haut-de-page"class="rounded-2xl">
-    </button>
+        <button class="scroll-to-top-button">
+            <img src="img/fleche.png" alt="bouton-haut-de-page"class="rounded-2xl">
+        </button>
     </div>
     <script src="src/js/hautPage.js"></script>
 </footer>
