285413851215b0663333c54a656e5fb37400e43b
ChristineFAVIERE <147518683+ChristineFAVIERE@users.noreply.github.com>
2024-02-12 10:40:46 +0100

    add Mentions.php files

---
+++ Mentions.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Contact</title>
+</head>
+<body>
+    <!--appel au header-->
+    <?php
+    include('inc/header.php');
+    ?>
+    <!--Text Mentions Légales-->
+    <div class="mentionslegales">
+     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum molestias alias vitae obcaecati dolore et dolorum, itaque iste officia accusamus provident impedit ex tenetur amet mollitia ipsa! Nostrum, earum illum.  <p>
+        </p>
+    </div>
+    <!--appelle au footer-->
+    <?php
+    require_once('inc/footer.php');
+    ?>
+</body>
+</html>
